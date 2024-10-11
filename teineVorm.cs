b8ec089f1c18e1d8ca07229dbed923ab08999951
opilane <opilane@HP-CZC2349HT4>
2024-10-11 09:52:28 +0300

    zxc

--- teineVorm.cs
+++ teineVorm.cs
@@ -17,6 +17,8 @@ namespace WinFormsApp3
         Label lbl3;
         Label lbl4;
         Label lbl5;
+        Label lbl6;
+        NumericUpDown nud;
         public teineVorm(int x, int y)
         {
             this.Width = x;
@@ -44,32 +46,54 @@ namespace WinFormsApp3
             lbl3 = new Label();
             lbl3.Text = "?";
             lbl3.AutoSize = false;
-            lbl3.Size = new Size(60, 50);
+            lbl3.Size = new Size(20, 50);
             lbl3.Font = new Font("Arial", 18);
             lbl3.TextAlign = ContentAlignment.MiddleCenter;
-            lbl3.Location = new Point(50, 75);
+            lbl3.Location = new Point(15 + 100, 45);
             lbl3.Name = "plusLeftLabel";
 
-            lbl4 = new Label();
-            lbl4.Text = "+";
-            lbl4.AutoSize = false;
-            lbl4.Size = new Size(60, 50);
-            lbl4.Font = new Font("Arial", 18);
-            lbl4.TextAlign = ContentAlignment.MiddleCenter;
-            lbl4.Location = new Point(50, 75);
-            lbl4.Name = "plusRightLabel";
+            lbl6 = new Label();
+            lbl6.Text = "+";
+            lbl6.AutoSize = false;
+            lbl6.Size = new Size(20, 50);
+            lbl6.Font = new Font("Arial", 18);
+            lbl6.TextAlign = ContentAlignment.MiddleCenter;
+            lbl6.Location = new Point(55 + 100, 45);
+
 
             lbl4 = new Label();
-            lbl4.Text = "=";
+            lbl4.Text = "?";
             lbl4.AutoSize = false;
-            lbl4.Size = new Size(60, 50);
+            lbl4.Size = new Size(20, 50);
             lbl4.Font = new Font("Arial", 18);
             lbl4.TextAlign = ContentAlignment.MiddleCenter;
-            lbl4.Location = new Point(50, 75);
+            lbl4.Location = new Point(95 + 100, 45);
             lbl4.Name = "plusRightLabel";
+
+            lbl5 = new Label();
+            lbl5.Text = "=";
+            lbl5.AutoSize = false;
+            lbl5.Size = new Size(20, 50);
+            lbl5.Font = new Font("Arial", 18);
+            lbl5.TextAlign = ContentAlignment.MiddleCenter;
+            lbl5.Location = new Point(135 + 100, 45);
+
+
+            nud = new NumericUpDown();
+            nud.Font = new Font("Arial", 18);
+            nud.MaximumSize = new Size(115, 100);
+            nud.Location = new Point(165 + 100, 55);
+  
+            nud.Name = "sum";
+
 
             this.Controls.Add(lbl);
             this.Controls.Add(lbl2);
+            this.Controls.Add(lbl3);
+            this.Controls.Add(lbl4);
+            this.Controls.Add(lbl5);
+            this.Controls.Add(lbl6);
+            this.Controls.Add(nud);
 
         }
     }
